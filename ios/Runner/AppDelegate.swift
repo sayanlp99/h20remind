00b18fa8f6758598313468be370f5fdf22827d96
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2021-12-11 18:30:14 +0300

    iOS configurations

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,18 +3,19 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  if #available(iOS 10.0, *) {
-    UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-  }
-  if(!UserDefaults.standard.bool(forKey: "Notification")) {
-    UIApplication.shared.cancelAllLocalNotifications()
-    UserDefaults.standard.set(true, forKey: "Notification")
-}
+  
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+    if(!UserDefaults.standard.bool(forKey: "Notification")) {
+      UIApplication.shared.cancelAllLocalNotifications()
+      UserDefaults.standard.set(true, forKey: "Notification")
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
