1d08962d73f82cc253bebc063bfaa50ef05f0355
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2021-12-17 18:15:57 +0530

    Fix bug for web

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:h20remind/screens/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-Future<void> main(context) async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
