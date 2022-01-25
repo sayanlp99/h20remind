5b88690d6ac3b44bbe4285633f0a068f4fbd7d51
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2022-01-25 22:03:01 +0530

    testing

--- lib/screens/report.dart
+++ lib/screens/report.dart
@@ -1,4 +1,10 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
+import 'package:h20remind/constants.dart';
+import 'package:h20remind/screens/dashboard.dart';
+import 'package:h20remind/widgets/h20remind_drawer.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class Report extends StatefulWidget {
   Report({Key? key}) : super(key: key);
@@ -8,8 +14,141 @@ class Report extends StatefulWidget {
 }
 
 class _ReportState extends State<Report> {
+  late final ValueNotifier<List<Event>> _selectedEvents;
+  final ValueNotifier<DateTime> _focusedDay = ValueNotifier(DateTime.now());
+  RangeSelectionMode _rangeSelectionMode = RangeSelectionMode.toggledOff;
+  DateTime? _rangeStart;
+  DateTime? _rangeEnd;
+  final Set<DateTime> _selectedDays = LinkedHashSet<DateTime>(
+    equals: isSameDay,
+    hashCode: getHashCode,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+
+    _selectedDays.add(_focusedDay.value);
+    _selectedEvents = ValueNotifier(_getEventsForDay(_focusedDay.value));
+  }
+
+  @override
+  void dispose() {
+    _focusedDay.dispose();
+    _selectedEvents.dispose();
+    super.dispose();
+  }
+
+  List<Event> _getEventsForDay(DateTime day) {
+    return kEvents[day] ?? [];
+  }
+
+  List<Event> _getEventsForDays(Iterable<DateTime> days) {
+    return [
+      for (final d in days) ..._getEventsForDay(d),
+    ];
+  }
+
+  List<Event> _getEventsForRange(DateTime start, DateTime end) {
+    final days = daysInRange(start, end);
+    return _getEventsForDays(days);
+  }
+
+  void _onRangeSelected(DateTime? start, DateTime? end, DateTime focusedDay) {
+    setState(() {
+      _focusedDay.value = focusedDay;
+      _rangeStart = start;
+      _rangeEnd = end;
+      _selectedDays.clear();
+      _rangeSelectionMode = RangeSelectionMode.toggledOn;
+    });
+
+    if (start != null && end != null) {
+      _selectedEvents.value = _getEventsForRange(start, end);
+    } else if (start != null) {
+      _selectedEvents.value = _getEventsForDay(start);
+    } else if (end != null) {
+      _selectedEvents.value = _getEventsForDay(end);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      drawer: h20remindDrawer(context),
+      appBar: AppBar(
+        title: const Text('Report'),
+        flexibleSpace: Container(
+          color: Colors.lightBlueAccent,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const Dashboard()));
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            children: [
+              TableCalendar(
+                focusedDay: kToday,
+                firstDay: kFirstDay,
+                lastDay: kLastDay,
+                onDaySelected: _onDaySelected,
+              ),
+              const SizedBox(height: 8.0),
+              Expanded(
+                child: ValueListenableBuilder<List<Event>>(
+                  valueListenable: _selectedEvents,
+                  builder: (context, value, _) {
+                    return ListView.builder(
+                      itemCount: value.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: const EdgeInsets.symmetric(
+                            horizontal: 12.0,
+                            vertical: 4.0,
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(),
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                          child: ListTile(
+                            onTap: () => print('${value[index]}'),
+                            title: Text('${value[index]}'),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      if (_selectedDays.contains(selectedDay)) {
+        _selectedDays.remove(selectedDay);
+      } else {
+        _selectedDays.add(selectedDay);
+      }
+
+      _focusedDay.value = focusedDay;
+      _rangeStart = null;
+      _rangeEnd = null;
+      _rangeSelectionMode = RangeSelectionMode.toggledOff;
+    });
+
+    _selectedEvents.value = _getEventsForDays(_selectedDays);
   }
 }
